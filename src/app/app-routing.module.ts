3d36b97e647770319bcccde33b375ee371cc928b
Emrys Corbin <emrys@corbin.dev>
2021-09-06 20:51:36 -0500

    Add basic temp routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+  // Registration & Login
+  { path: 'login', component: PageNotFoundComponent },
+  { path: 'register', component: PageNotFoundComponent },
+
+  { path: 'user/:id', component: PageNotFoundComponent },
+  { path: 'user/settings', component: PageNotFoundComponent },
+
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
